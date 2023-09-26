3ec1696d2c707525a032301c759b1d2328d9fb2f
vyshnavsmenon <vyshnavsmenon62@gmail.com>
2023-09-27 03:26:46 +0530

    logout css file done successfully

--- src/Logout.js
+++ src/Logout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useCookies } from 'react-cookie'
 import { useNavigate } from 'react-router-dom';
+import './Logout.css'
 
 function Logout() {
     const [cookies, , removeCookie] = useCookies(['user-id']);
@@ -10,9 +11,19 @@ function Logout() {
         removeCookie('user-id');
         navigate('/login');
     }
+
+    function redirect(){
+      navigate('/');
+    }
   return (
-    <div>
-        <button onClick={handleLogout}>Logout</button>
+    <div className='background'>
+      <div className='dialogue-box'>
+          <p>Are you sure you want to Logout?</p>
+          <div className='buttons'>
+            <button onClick={handleLogout}>Yes</button>
+            <button onClick={redirect}>No</button>
+          </div>
+      </div>        
     </div>
   )
 }
